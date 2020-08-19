b72e9309b350696709d0fccd120defe8a7bdcc4c
Manno <manno-xx@users.noreply.github.com>
2020-08-19 21:50:33 +0200

    Some animation using a sprite to draw attention to LED when radio signal comes in

--- main.ts
+++ main.ts
@@ -1,18 +1,42 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    basic.showLeds(`
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        `)
-    basic.pause(100)
-    strength = Math.round(Math.map(radio.receivedPacket(RadioPacketProperty.SignalStrength), -128, -42, 20, 0))
-    basic.showString("" + (strength))
+    blinkForAttention()
+    signalStrength = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+    approximateDistance = Math.round(Math.map(signalStrength, -128, -42, 8, 0))
+    basic.showString("" + (approximateDistance))
 })
-let strength = 0
-radio.setGroup(1)
-basic.showString("" + (input.temperature()))
+input.onButtonPressed(Button.A, function () {
+    BTChannel = BTChannel + 1
+    BTChannel = BTChannel % 255
+    basic.showString("" + (BTChannel))
+})
+input.onButtonPressed(Button.B, function () {
+    BTChannel = BTChannel - 1
+    BTChannel = BTChannel % 255
+    basic.showString("" + (BTChannel))
+})
+function blinkForAttention () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    blipSprite = game.createSprite(4, 2)
+    basic.pause(50)
+    for (let index = 0; index < 4; index++) {
+        blipSprite.move(-1)
+        basic.pause(50)
+    }
+    blipSprite.delete()
+    basic.pause(50)
+}
+let blipSprite: game.LedSprite = null
+let approximateDistance = 0
+let signalStrength = 0
+let BTChannel = 0
+BTChannel = 1
+radio.setGroup(BTChannel)
 basic.pause(2000)
 basic.forever(function () {
     radio.sendNumber(1)
